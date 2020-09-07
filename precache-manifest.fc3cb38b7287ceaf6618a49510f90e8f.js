074d1e770b4a046ea2cc218496db56f820e26d6f
Aldrin Castro <aldrin.castro@regovtech.com>
2020-09-07 22:37:54 +0800

    Updates

---
+++ precache-manifest.fc3cb38b7287ceaf6618a49510f90e8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573c855b4c55389b2a2c4b34b99317a5",
+    "url": "/receipt-generation/index.html"
+  },
+  {
+    "revision": "fb53f55b357900ecbc2a",
+    "url": "/receipt-generation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6edb5dbdc703e2d262b0",
+    "url": "/receipt-generation/static/js/2.e8f80d8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/receipt-generation/static/js/2.e8f80d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb53f55b357900ecbc2a",
+    "url": "/receipt-generation/static/js/main.a62ad08c.chunk.js"
+  },
+  {
+    "revision": "48c7a8f70e59c4a01866",
+    "url": "/receipt-generation/static/js/runtime-main.63980370.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/receipt-generation/static/media/logo.5d5d9eef.svg"
+  }
+]);
